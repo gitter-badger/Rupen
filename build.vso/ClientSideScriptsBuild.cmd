7aeac6c90f44e6b3b1fffe2c0cd0b31d46abc439
cilerler <cilerler@hotmail.com>
2015-12-13 20:21:03 -0500

    gulp added into local script

--- build.vso/ClientSideScriptsBuild.cmd
+++ build.vso/ClientSideScriptsBuild.cmd
@@ -1,4 +1,11 @@
-ECHO %CD%
+REM @ECHO OFF
+SETLOCAL ENABLEEXTENSIONS
+SET me=%~n0
+SET parent=%~dp0
 
-CALL ".\node_modules\.bin\bower.cmd" install
-CALL ".\node_modules\.bin\tsd.cmd" install
+SET GULP_TASK=%1
+SET GULP_FILE=%2
+SET NODEMODULES=.\node_modules\.bin\
+CALL %NODE_MODULES%gulp.cmd %GULP_TASK% --gulpfile %GULP_FILE%
+CALL %NODE_MODULES%bower.cmd install
+CALL %NODE_MODULES%tsd.cmd reinstall --save --overwrite
