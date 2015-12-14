79d671c981f37cd6cd4afc4d660a8c571a5ed47d
cilerler <cilerler@hotmail.com>
2015-12-13 21:05:06 -0500

    Minor structure changes

---
+++ build.client/Initialize.cmd
@@ -0,0 +1,5 @@
+@REM CALL npm install -g bower tsd gulp
+CALL npm install
+CALL bower install
+@REM CALL tsd install
+CALL tsd reinstall --save --overwrite
--- build.vso/BuildClientSideScripts.cmd
+++ build.vso/BuildClientSideScripts.cmd
@@ -5,6 +5,6 @@ SET parent=%~dp0
 
 SET GULP_TASK=%1
 SET GULP_FILE=%2
-CALL ".\node_modules\.bin\gulp.cmd" %GULP_TASK% --gulpfile %GULP_FILE%
-CALL ".\node_modules\.bin\bower.cmd" install
 CALL ".\node_modules\.bin\tsd.cmd" reinstall --save --overwrite
+CALL ".\node_modules\.bin\bower.cmd" install
+CALL ".\node_modules\.bin\gulp.cmd" %GULP_TASK% --gulpfile %GULP_FILE%
