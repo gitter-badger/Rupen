93ba92b35c64b27b76a34f0d013b886abbdea7be
cilerler <cilerler@hotmail.com>
2015-12-13 20:50:40 -0500

    paths fixed

--- build.vso/ClientSideScriptsBuild.cmd
+++ build.vso/ClientSideScriptsBuild.cmd
@@ -5,7 +5,6 @@ SET parent=%~dp0
 
 SET GULP_TASK=%1
 SET GULP_FILE=%2
-SET NODEMODULES=.\node_modules\.bin\
-CALL %NODE_MODULES%gulp.cmd %GULP_TASK% --gulpfile %GULP_FILE%
-CALL %NODE_MODULES%bower.cmd install
-CALL %NODE_MODULES%tsd.cmd reinstall --save --overwrite
+CALL ".\node_modules\.bin\gulp.cmd" %GULP_TASK% --gulpfile %GULP_FILE%
+CALL ".\node_modules\.bin\bower.cmd" install
+CALL ".\node_modules\.bin\tsd.cmd" reinstall --save --overwrite
