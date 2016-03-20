f796aa86e5c873b56fdd4bc3af5e31f19d223cb6
Cengiz Ilerler <cilerler@gmail.com>
2016-03-20 10:09:19 -0400

    Update ApplyVersionToAssemblies.ps1

--- build.vso/ApplyVersionToAssemblies.ps1
+++ build.vso/ApplyVersionToAssemblies.ps1
@@ -1,36 +1,46 @@
-function ApplyVersionToAssemblies
+function ReplaceVersion([string]$BuildSourcePath, [string] $versionFile, [string] $regexPattern, [string]$finalVersion)
 {
-    [CmdletBinding()]
-    Param
-    (
-		[parameter(Position=0, Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-		[string]$BuildSourcePath,
-		[parameter(Position=1, Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        [string]$BuildBuildNumber
-    )
-
-	# Regular expression pattern to find the version in the build number
-	# and then apply it to the assemblies
-	$VersionRegex = "\d+\.\d+\.\d+\.\d+"
-
-	# Apply the version to the assembly property files
-	$files = Get-ChildItem $BuildSourcePath -recurse -include "*AssemblyInfo.cs"
-
-	if($files)
-	{
+    [bool]$output = $false
+    $files = Get-ChildItem $BuildSourcePath -recurse -include $versionFile
+    if ($files)
+    {
+        $output = $true
 		Write-Host "Will apply $BuildBuildNumber to $($files.count) files in $BuildSourcePath"
 
 		foreach ($file in $files) {
 			$filecontent = Get-Content($file)
 			attrib $file -r
-			$filecontent -replace $VersionRegex, $BuildBuildNumber | Out-File $file
+			$filecontent -replace $regexPattern, $finalVersion | Out-File $file
 			Write-Verbose "$file.FullName - version applied"
 		}
-	}
-	else
+    }
+    return $output
+}
+
+function ApplyVersionToAssemblies
+{
+    [CmdletBinding()]
+    Param
+    (
+	[parameter(Position=0, Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+	[string]$BuildSourcePath,
+	[parameter(Position=1, Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$BuildBuildNumber
+    )
+
+	$replaceSuccess = (ReplaceVersion $BuildSourcePath "project.json" '\s*\"version\"\s*:\s*\"\d+\.\d+\.\d+-\*?(\w*)\"' '  "version": "' + $BuildBuildNumber + '"')
+	if ($replaceSuccess = $false)
+	{   
+	    $replaceSuccess = (ReplaceVersion $BuildSourcePath "*AssemblyInfo.cs" "\d+\.\d+\.\d+\.\d+" $BuildBuildNumber)
+	    if ($replaceSuccess = $false)
+	    {
+	        Write-Warning "No file found at $BuildSourcePath"
+	    }
+	} 
+	else 
 	{
-		Write-Warning "No file found at $BuildSourcePath"
+    	    Write-Verbose "DNX project detected, skipping..."
 	}
 }
